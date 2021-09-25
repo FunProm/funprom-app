0673a4fd73c178da8e1ca16e035032c094a1c5f4
Lino Lendi <lino.lendi@husqvarnagroup.com>
2021-09-25 03:10:08 +0200

    add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
+    import { Router, Link, Route } from "svelte-routing";
     import Login from "./Login.svelte";
     import Logout from "./Logout.svelte";
     import Tutorial from "./Tutorial.svelte";
+    import { navigate } from "svelte-routing";
+    export let url = "";
 
     let user = {loggedIn: false};
 
     function handleLogin(event) {
         console.log(event.detail.text);
         user.loggedIn = true;
+        navigate("/tutorial", { replace: true });
     }
 
     function handleLogout(event) {
         console.log(event.detail.text);
         user.loggedIn = false;
+        navigate("/", { replace: true });
     }
-
 </script>
 
-{#if !user.loggedIn}
-    <Login on:message={handleLogin}/>
-{:else}
-    <Logout on:message={handleLogout}/>
-    <Tutorial/>
-{/if}
+<Router url="{url}">
+    <Route path="/"><Login on:message={handleLogin}/></Route>
+    <Route path="/tutorial"><Tutorial/></Route>
+    <Route path="/logout"><Logout on:message={handleLogout}/></Route>
+</Router>
