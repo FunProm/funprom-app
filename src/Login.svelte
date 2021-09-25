f5e7a934894dcc266a4d7dfba851443bb31a4a97
Lino Lendi <lino.lendi@husqvarnagroup.com>
2021-09-25 14:45:38 +0200

    rework tutorial

--- src/Login.svelte
+++ src/Login.svelte
@@ -29,9 +29,6 @@
 
 <ion-header translucent="true">
     <ion-toolbar>
-        <ion-buttons slot="start">
-            <ion-menu-button></ion-menu-button>
-        </ion-buttons>
         <ion-title>Login</ion-title>
     </ion-toolbar>
 </ion-header>
--- src/Tutorial.svelte
+++ src/Tutorial.svelte
@@ -11,53 +11,54 @@
 </svelte:head>
 <ion-header translucent="true">
     <ion-toolbar>
-        <ion-buttons slot="start">
-            <ion-menu-button></ion-menu-button>
-        </ion-buttons>
         <ion-title>Tutorial</ion-title>
     </ion-toolbar>
 </ion-header>
 
 <ion-slides pager="true">
     <ion-slide>
+        <ion-toolbar>
+            <div class="ion-text-end">
+                <button ion-button color="primary" on:click="{handleTutorialEnd}">Skip</button>
+            </div>
+        </ion-toolbar>
         <img src="../assets/img/slides/slide-1.png" alt="pict"/>
         <h2>Welcome!</h2>
-        We are happy to see you. <br/>
-        <br/>
-        <br/>
+        <p>We are happy to see you.</p>
     </ion-slide>
 
     <ion-slide>
+        <ion-toolbar>
+            <div class="ion-text-end">
+                <button ion-button color="primary" on:click="{handleTutorialEnd}" >Skip</button>
+            </div>
+        </ion-toolbar>
         <img src="../assets/img/slides/svelte.png" alt="pict"/>
         <h2>Handy tool!</h2>
-        Prom can be burdensome, therefore <b>FunProm</b> wants to make it easier for you. Just swipe to answer the
-        questions.
-        <br/>
-        Additionally all answers get saved, so you can stop and come back whenever you wish<br/>
-        <br/>
-        <br/>
+        <p>Prom can be burdensome, therefore <b>FunProm</b> wants to make it easier for you. Just swipe to answer the
+            questions.
+            <br/>
+            Additionally all answers get saved, so you can stop and come back whenever you wish</p>
     </ion-slide>
 
     <ion-slide>
+        <div class="ion-text-end">
+            <button ion-button color="primary" on:click="{handleTutorialEnd}">Skip</button>
+        </div>
         <img src="../assets/img/slides/slide-2.png" alt="pict"/>
         <h2>What are the benefits of answering the questions?</h2>
-
-        Research shows, that it can help you to get well soon and of course it's fun <br/>
-        <br/>
-        <br/>
+        <p>Research shows, that it can help you to get well soon and of course it's fun</p>
     </ion-slide>
 
     <ion-slide>
         <img src="../assets/img/slides/slide-4.png" alt="pict"/>
         <h2>Ready to Play?</h2>
-        Check out your progress to get an overview. <br/>
-        <ion-button on:click="{handleTutorialEnd}" fill="clear">
-            Or directly continue to earn points
-            <ion-icon slot="end" name="arrow-forward"></ion-icon>
-        </ion-button>
-        <br/>
-        <br/>
-        <br/>
+        <p>Check out your progress to get an overview. <br/>
+            <ion-button on:click="{handleTutorialEnd}" fill="clear">
+                Or directly continue to earn points
+                <ion-icon slot="end" name="arrow-forward"></ion-icon>
+            </ion-button>
+        </p>
     </ion-slide>
 </ion-slides>
 
@@ -79,5 +80,12 @@
         max-height: 50%;
         max-width: 60%;
         margin: 36px 0;
+    }
+
+    ion-slide > p {
+        padding: 0 40px;
+        font-size: 14px;
+        line-height: 1.5;
+        color: #60646B;
     }
 </style>
